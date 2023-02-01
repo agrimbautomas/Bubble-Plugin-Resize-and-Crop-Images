337f3b3722ccdc88a84d75972da8bda66f7404a1
Tomas Agrimbau <tomas@amalgama.co>
2023-02-01 02:18:11 -0300

    Automatic commit from Bubble

--- elements/AAE-850ma/initialize.js
+++ elements/AAE-850ma/initialize.js
@@ -1,27 +1,27 @@
 function(instance, context) {
+    
+	instance.canvas.append('<label id="bleap-file-upload"><input type="file" id="bleap-uploader"><img><span>Click to upload an image</span></label><span id="crop-img-btn" data-method="getCroppedCanvas"></span>');
 
-        instance.canvas.append('<label id="bleap-file-upload"><input type="file" id="bleap-uploader"><img><span>Click to upload an image</span></label><span id="crop-img-btn" data-method="getCroppedCanvas"></span>');
-    
-    
-	$(document).on('click', '#bleap-cropper-popup-save-button', function () {
-		uploadCroppedImage()
-	});
-    
+
+    $(document).on('click', '#bleap-cropper-popup-save-button', function () {
+        uploadCroppedImage()
+    });
+
     function uploadCroppedImage(){
         let fileName = BleapCropper.getUploadedImageName();
         const imgInBase64 = BleapCropper.getImgInBase64();
         const tmp = imgInBase64.split(',')
-		const data = tmp[1]
-        
+        const data = tmp[1]
+
         context.uploadContent(fileName, data, uploadCallback);
     }
-    
+
     function uploadCallback(err, url) {
         if (url)        
             instance.triggerEvent('cropping_finishes');
-		else 
-        	console.log('callback error: ' + err)
-    	
-	}
+        else 
+            console.log('callback error: ' + err)
+
+    }
     
 }
--- elements/AAE-850ma/update.js
+++ elements/AAE-850ma/update.js
@@ -1,3 +1,5 @@
 function(instance, properties, context) {
-
+    const ASPECT_RATIO_WIDTH = properties.aspect_ratio_width;
+    const ASPECT_RATIO_HEIGHT = properties.aspect_ratio_height;
+    BleapCropper.setOptions(ASPECT_RATIO_WIDTH, ASPECT_RATIO_HEIGHT);
 }
